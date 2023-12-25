342211fd5c30df801f02bd7d26c0a91307f50416
Mehdi BHA <mehdibenhadjali@gmail.com>
2023-12-25 20:08:54 +0100

    fix: update app url

--- apps/marketing/src/config/constants.ts
+++ apps/marketing/src/config/constants.ts
@@ -1,4 +1,4 @@
 export const APP_URL =
   process.env.NODE_ENV === "development"
     ? `http://localhost:3000`
-    : `https://turbocharger-app.mehdibha.com`;
+    : `https://app.turbocharger.com`;
