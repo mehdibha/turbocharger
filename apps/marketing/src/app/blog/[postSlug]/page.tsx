b2db7a762d9aa4ed77670251c2cc19c4ad44af3d
Mehdi BHA <mehdibenhadjali@gmail.com>
2023-12-26 16:57:45 +0100

    feat: blog feature

--- apps/marketing/src/app/blog/[postSlug]/page.tsx
+++ apps/marketing/src/app/blog/[postSlug]/page.tsx
@@ -1,18 +1,24 @@
-import React from "react";
-import type { Metadata } from "next";
 import { notFound } from "next/navigation";
+import { serialize } from "next-mdx-remote/serialize";
+import { Badge } from "@turbocharger/ui";
+import { formatDate } from "@turbocharger/utils";
+import { MDX } from "@/components/mdx";
+import { getPostBySlug } from "@/lib/posts";
 
-async function getPost(postSlug: string) {
-  return {
-    post: null,
-  };
-}
+// export async function generateMetadata({ params }: PostPageProps): Promise<Metadata> {
+//   const { post } = await getPostBySlug(params.postSlug);
 
-export async function generateStaticParams() {
-  // TODO: fix this
+//   if (!post) {
+//     return {};
+//   }
 
-  return [];
-}
+//   return {
+//     title: post.title,
+//     openGraph: {
+//       images: post?.thumbnail ? [post.thumbnail] : undefined,
+//     },
+//   };
+// }
 
 interface PostPageProps {
   params: {
@@ -20,43 +26,28 @@ interface PostPageProps {
   };
 }
 
-export async function generateMetadata({ params }: PostPageProps): Promise<Metadata> {
-  const { post } = await getPost(params.postSlug);
-
-  // if (!post) {
-  //   return {};
-  // }
-
-  return {
-    title: "post.title",
-    openGraph: {
-      images: "post?.thumbnail ? [post.thumbnail] : undefined",
-    },
-  };
-}
-
-const PostPage = async (props: PostPageProps) => {
+export default async function PostPage(props: PostPageProps) {
   const { params } = props;
-  const { postSlug } = params;
-  const { post } = await getPost(postSlug);
+  const post = getPostBySlug(params.postSlug);
 
-  if (!post) notFound();
+  if (!post) {
+    notFound();
+  }
 
   return (
     <div className="mt-20">
-      {/* <h1 className="mb-4 text-4xl font-bold">{post.title}</h1>
+      <h1 className="mb-4 text-4xl font-bold">{post.metadata.title}</h1>
       <div className="flex items-center space-x-2">
-        <p>Published in {formatDate(post.createdTime)}</p>
+        <p>Published in {formatDate(post.metadata.publishedAt)}</p>
         <p>•</p>
-        <p>3 min read</p>
+        <p>{post.timeToRead} min read</p>
         <p>•</p>
         <div className="flex space-x-1">
-          {post.tags && post.tags.map((tag) => <Badge key={tag}>{tag}</Badge>)}
+          {post.metadata.keywords &&
+            post.metadata.keywords.map((tag) => <Badge key={tag}>{tag}</Badge>)}
         </div>
-      </div> */}
-      <div className="mt-12">{/* TODO: post here */}</div>
+      </div>
+      <article className="mt-8">{post.content && <MDX source={post.content} />}</article>
     </div>
   );
-};
-
-export default PostPage;
+}
--- apps/marketing/src/app/blog/page.tsx
+++ apps/marketing/src/app/blog/page.tsx
@@ -1,5 +1,8 @@
 import { Metadata } from "next";
+import { getAllPosts } from "@/lib/posts";
+// import { getBlogPosts } from "@/lib/blog";
 import { siteConfig } from "@/config";
+import { PostsExplorer } from "./posts-explorer";
 
 const config = siteConfig.blogPage;
 
@@ -8,12 +11,16 @@ export const metadata: Metadata = {
 };
 
 export default async function BlogPage() {
+  const posts = getAllPosts();
+  console.log(posts);
+
   return (
     <div className="container max-w-7xl pt-12">
       <h1 className="font-display max-w-2xl text-3xl font-semibold sm:text-4xl md:text-5xl">
         {config.headline}
       </h1>
       <p className="text-muted-foreground mt-2 text-lg">{config.subheadline}</p>
+      <PostsExplorer posts={posts} />
     </div>
   );
 }
